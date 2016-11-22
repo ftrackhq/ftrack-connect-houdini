704f84bceb6999013897d7722cfe7a091e2253b7
Mike Datsik <m.datcik@postmodern.ua>
2016-11-22 13:18:48 +0200

    Mod

--- resource/houdini_path/scripts/python/ftrack_startup.py
+++ resource/houdini_path/scripts/python/ftrack_startup.py
@@ -1,16 +1,7 @@
-# -*- coding: utf-8 -*-
-# @Author: datsik
-# @Date:   2016-07-13 15:08:58
-# @Last Modified by:   datsik
-# @Last Modified time: 2016-07-13 18:25:35
-
-
-# import sys
 import os
 import hou
 import ftrack
-
-# import functools
+import tempfile
 
 from ftrack_connect.ui.widget.import_asset import FtrackImportAssetDialog
 from ftrack_connect.ui.widget.asset_manager import FtrackAssetManagerDialog
@@ -22,12 +13,43 @@ from ftrack_connect_houdini.ui.publisher import PublishAssetDialog
 from ftrack_connect_houdini.ui.tasks import FtrackTasksDialog
 
 connector = Connector()
+connector.registerAssets()
 
 currentEntity = ftrack.Task(
     os.getenv('FTRACK_TASKID',
               os.getenv('FTRACK_SHOTID')
               )
 )
+
+
+def writePypanel(pan_name, pan_start):
+
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<pythonPanelDocument>
+  <interface name="{0}" label="{0}" icon="MISC_python" help_url="">
+    <script><![CDATA[
+
+import ftrack_startup
+
+def createInterface():
+
+    info_view = ftrack_startup.{1}()
+
+    return info_view]]></script>
+    <help><![CDATA[]]></help>
+  </interface>
+</pythonPanelDocument>"""
+
+    xml = xml.format(pan_name, pan_start)
+
+    path = os.path.join(tempfile.gettempdir(), '%s.pypanel' % pan_name)
+    if os.path.exists(path):
+        pass
+    else:
+        f = open(path, 'w')
+        f.write(xml)
+        f.close()
+    return path
 
 
 def FtrackDialogs(panel_id):
@@ -50,8 +72,6 @@ def FtrackDialogs(panel_id):
         panel = main_tab.pane().createTab(hou.paneTabType.PythonPanel)
         panel.showToolbar(False)
         panel.setActiveInterface(panel_interface)
-
-        print 'This is Starta'
     else:
         if panel_interface:
             hou.hscript("pane -S -m pythonpanel -o -n %s" % ftrack_id)
@@ -73,19 +93,20 @@ def showTaskDialog():
 
 
 def showImportAssetDialog():
+
     dialog = FtrackImportAssetDialog(connector=connector)
 
     return dialog
 
 
 def showAssetManagerDialog():
+    connector = Connector()
     dialog = FtrackAssetManagerDialog(connector=connector)
 
     return dialog
 
 
 def showPublishDialog():
-    connector.registerAssets()
 
     dialog = PublishAssetDialog(
         currentEntity=currentEntity, connector=connector)
