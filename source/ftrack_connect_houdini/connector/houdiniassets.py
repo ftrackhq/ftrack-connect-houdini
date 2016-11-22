062c4df5d536fe49d7c9e6fbfef90ea00cdc1d9d
Mike Datsik <m.datcik@postmodern.ua>
2016-11-22 18:44:50 +0200

    New Connector script

--- source/ftrack_connect_houdini/connector/houdiniassets.py
+++ source/ftrack_connect_houdini/connector/houdiniassets.py
@@ -21,29 +21,40 @@ class GenericAsset(FTAssetType):
         self.referenceAssetBool = False
 
     def importAsset(self, iAObj=None):
-        if iAObj.componentName == 'alembic':
-            pass
+
+        if 'alembic' in iAObj.componentName:
+            resultingNode = hou.node('/obj').createNode(
+                'alembicarchive', iAObj.assetName)
+            resultingNode.parm('buildSubnet').set(False)
+            resultingNode.parm('fileName').set(iAObj.filePath)
+            hou.hscript(
+                "opparm -C {0} buildHierarchy (1)".format(
+                    resultingNode.path()))
+        elif 'bgeo' in iAObj.componentName:
+            geoNode = hou.node('/obj').createNode('geo')
+            fileSop = geoNode.children()[0]
+            fileSop.parm('file').set(iAObj.filePath)
+
         else:
             self.importAssetBool = False
-            nameSpaceStr = iAObj.options.get('nameSpaceStr', None) or iAObj.assetType.upper()
-            importType = 'houdiniBinary'
             hou.hipFile.load(iAObj.filePath)
+
+        self.addFTab(resultingNode)
+        self.setFTab(resultingNode, iAObj)
 
         return 'Imported ' + iAObj.assetType + ' asset'
 
-
     def publishAsset(self, iAObj=None):
+        '''Publish the asset defined by the provided *iAObj*.'''
+
+        publishedComponents = []
+
         panelComInstance = panelcom.PanelComInstance.instance()
 
         if hasattr(iAObj, 'customComponentName'):
             componentName = iAObj.customComponentName
         else:
             componentName = 'houdiniBinary'
-
-        # channels = True
-
-        # if 'houdiniHistory' in iAObj.options and iAObj.options['houdiniHistory']:
-        #     constructionHistory = iAObj.options['houdiniHistory']
 
         publishedComponents = []
 
@@ -53,15 +64,53 @@ class GenericAsset(FTAssetType):
             FTComponent(
                 componentname=componentName, path=temporaryPath))
 
-        if 'exportMode' in iAObj.options and iAObj.options['exportMode'] == 'Selection':
-            print "Fuck"
+        if 'exportMode' in iAObj.options and (
+                iAObj.options['exportMode'] == 'Selection'):
+            pass
         else:
             hou.hipFile.save(temporaryPath)
-
 
         panelComInstance.emitPublishProgressStep()
 
         return publishedComponents, 'Published ' + iAObj.assetType + ' asset'
+
+    def addFTab(self, resultingNode):
+        parm_group = resultingNode.parmTemplateGroup()
+        parm_folder = hou.FolderParmTemplate('folder', 'ftrack')
+        alembic_folder = parm_group.findFolder('Alembic Loading Parameters')
+
+        components = [
+            'componentId', 'componentName', 'assetVersionId',
+            'assetVersion', 'assetName', 'assetType', 'assetId'
+        ]
+
+        for comp in components:
+            parm_folder.addParmTemplate(
+                hou.StringParmTemplate(comp, comp, 1, ''))
+        if alembic_folder:
+            parm_group.insertAfter(alembic_folder, parm_folder)
+        else:
+            parm_group.append(parm_folder)
+        resultingNode.setParmTemplateGroup(parm_group)
+
+    def setFTab(self, resultingNode, iAObj):
+        componentId = ftrack.Component(
+            iAObj.componentId).getEntityRef()
+        assetVersionId = ftrack.AssetVersion(
+            iAObj.assetVersionId).getEntityRef()
+
+        components = {
+            'componentId': HelpFunctions.safeString(componentId),
+            'componentName': HelpFunctions.safeString(iAObj.componentName),
+            'assetVersionId': HelpFunctions.safeString(assetVersionId),
+            'assetVersion': HelpFunctions.safeString(iAObj.assetVersion),
+            'assetName': HelpFunctions.safeString(iAObj.assetName),
+            'assetType': HelpFunctions.safeString(iAObj.assetType),
+            'assetId': HelpFunctions.safeString(iAObj.assetId)
+        }
+
+        for comp in components:
+            resultingNode.parm(comp).set(components[comp])
 
     def getSceneSettingsObj(self, iAObj):
         iAObjCopy = copy.copy(iAObj)
@@ -71,7 +120,16 @@ class GenericAsset(FTAssetType):
         return iAObjCopy
 
     def changeVersion(self, iAObj=None, applicationObject=None):
-        pass
+        for n in hou.node('/').allSubChildren():
+            if n.name().startswith(applicationObject):
+                print iAObj.filePath
+                n.parm('fileName').set(
+                    HelpFunctions.safeString(iAObj.filePath))
+                hou.hscript(
+                    "opparm -C {0} buildHierarchy (1)".format(
+                        n.path()))
+                self.setFTab(n, iAObj)
+                return True
 
 
 class SceneAsset(GenericAsset):
@@ -104,15 +162,41 @@ class SceneAsset(GenericAsset):
     @staticmethod
     def exportOptions():
         xml = """
-        <tab name="Options">
-            <row name="Export" accepts="houdini">
+        <tab name="Houdini Scene options" accepts="houdini">
+            <row name="Houdini Selection Mode" accepts="houdini">
                 <option type="radio" name="exportMode">
                         <optionitem name="All" value="True"/>
+                        <optionitem name="Selection"/>
+                </option>
+            </row>
+        </tab>
+        <tab name="Alembic options">
+            <row name="Publish Alembic">
+                <option type="checkbox" name="alembic"/>
+            </row>
+            <row name="Include animation">
+                <option type="checkbox" name="alembicAnimation"/>
+            </row>
+            <row name="Frame range">
+                <option type="string" name="frameStart" value="{0}"/>
+                <option type="string" name="frameEnd" value="{1}"/>
+            </row>
+            <row name="Evaluate every">
+                <option type="float" name="alembicEval" value="1.0"/>
+            </row>
+            <row name="Alembic Selection Mode" accepts="maya">
+                <option type="radio" name="alembicExportMode">
+                        <optionitem name="All"/>
+                        <optionitem name="Selection" value="True"/>
                 </option>
             </row>
         </tab>
         """
+        s = os.getenv('FS')
+        e = os.getenv('FE')
+        xml = xml.format(s, e)
         return xml
+
 
 class GeometryAsset(GenericAsset):
     def __init__(self):
@@ -131,8 +215,27 @@ class GeometryAsset(GenericAsset):
 
         return publishedComponents, 'Published GeometryAsset asset'
 
+
+# class CameraAsset(GenericAsset):
+#     def __init__(self):
+#         super(GeometryAsset, self).__init__()
+
+#     def importAsset(self, iAObj=None):
+#         GenericAsset.importAsset(self, iAObj)
+
+#     def changeVersion(self, iAObj=None, applicationObject=None):
+#         return GenericAsset.changeVersion(self, iAObj, applicationObject)
+
+#     def publishAsset(self, iAObj=None):
+#         publishedComponents = []
+#         geometryComponents = GenericAsset.publishAsset(self, iAObj)
+#         publishedComponents += geometryComponents
+
+#         return publishedComponents, 'Published GeometryAsset asset'
+
+
 def registerAssetTypes():
     assetHandler = FTAssetHandlerInstance.instance()
     assetHandler.registerAssetType(name='geo', cls=GeometryAsset)
-    # assetHandler.registerAssetType(name='cam', cls=GenericAsset)
+    # assetHandler.registerAssetType(name='cam', cls=CameraAsset)
     assetHandler.registerAssetType(name='scene', cls=SceneAsset)
--- source/ftrack_connect_houdini/connector/houdinicon.py
+++ source/ftrack_connect_houdini/connector/houdinicon.py
@@ -3,17 +3,15 @@
 
 import os
 import uuid
+import urlparse
 
 import hou
 import toolutils
 
-# import houdini.cmds as mc
-# import houdini.OpenhoudiniUI as mui
-# import houdini.mel as mm
-# from pymel.core.uitypes import pysideWrapInstance
+import _alembic_hom_extensions as abc
 
 from ftrack_connect.connector import base as maincon
-from ftrack_connect.connector import FTAssetHandlerInstance
+from ftrack_connect.connector import FTAssetHandlerInstance, HelpFunctions
 
 
 class Connector(maincon.Connector):
@@ -21,13 +19,183 @@ class Connector(maincon.Connector):
         super(Connector, self).__init__()
 
     @staticmethod
+    def bakeAnim(
+            node, frameRange, bakeParms=[],
+            parentNode=hou.node('/obj'), byChop=False):
+
+        position = node.position()
+
+        # bake anim to keyframe by chop
+        if byChop:
+            tempChopnet = hou.node('/obj').createNode('chopnet')
+            objectChop = tempChopnet.createNode('object')
+            [objectChop.parm(k).set(v) for k, v in zip(
+                [
+                    'targetpath', 'compute',
+                    'samplerate', 'start', 'end', 'units'],
+                [
+                    node.path(), 'fullxform', hou.fps(),
+                    frameRange[0], frameRange[1], 'frames'])]
+
+        trsParms = ['tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz']
+
+        if node.type().name() == 'cam':
+            bakedNode = parentNode.createNode(
+                'cam', node_name=node.name() + "_bake")
+
+            [bakedNode.parm('resx').set(node.parm('resx').eval()),
+                bakedNode.parm('resy').set(node.parm('resy').eval())]
+            bakedNode.setPosition(
+                hou.Vector2((position[0] + 1, position[1] - 1)))
+
+        elif not node.type().name() == 'cam':
+            bakedNode = hou.copyNodesTo([node], parentNode)[0]
+            bakedNode.setInput(0, None)
+            [[bakedNode.parm(parmName).deleteAllKeyframes(),
+                bakedNode.parm(parmName).set(0)] for parmName in bakeParms]
+            bakedNode.setName('animation:' + node.name(), unique_name=True)
+            bakedNode.parm("keeppos").set(0)
+            bakedNode.movePreTransformIntoParmTransform()
+            bakedNode.setPosition(
+                hou.Vector2((position[0] + 1, position[1] + 1)))
+
+        for frame in xrange(int(frameRange[0]), int(frameRange[1]) + 1):
+            time = (frame - 1) / hou.fps()
+
+            tsrMatrix = node.worldTransformAtTime(time)
+            for parm, value in zip(
+                trsParms, (list(tsrMatrix.extractTranslates('srt')) + list(
+                    tsrMatrix.extractRotates(
+                        transform_order='srt', rotate_order='xyz')) + list(
+                    tsrMatrix.extractScales(transform_order='srt')))):
+                if parm in bakeParms:
+                    if not byChop:
+                        bakedNode.parm(parm).setKeyframe(
+                            hou.Keyframe(value, time))
+                    else:
+                        bakedNode.parm(parm).setKeyframe(
+                            hou.Keyframe(objectChop.track(
+                                parm).evalAtFrame(frame), time))
+
+            if bakeParms != []:
+                for parm, value in zip(
+                    bakeParms, [node.parm(p).evalAtFrame(
+                        frame) for p in bakeParms]):
+                    if parm not in trsParms:
+                        bakedNode.parm(parm).setKeyframe(
+                            hou.Keyframe(value, time))
+
+        if byChop:
+            tempChopnet.destroy()
+
+        return bakedNode
+
+    @staticmethod
+    def importCamFromAbc(
+            filepath, importAsBakedCam=False, scaleFactor=1, resolution=[]):
+        camXformPath = abc.alembicGetObjectPathListForMenu(filepath)[2]
+        camShapePath = abc.alembicGetObjectPathListForMenu(filepath)[4]
+
+        # sampleTime = hou.frame()/hou.fps()
+        trsName = camShapePath.split('/')[-2]
+        shapeName = camShapePath.split('/')[-1]
+
+        abcXform = hou.node('/obj/').createNode('alembicxform', trsName)
+        abcXform.parm('fileName').set(filepath)
+        abcXform.parm('frame').setExpression('$FF')
+        abcXform.parm('fps').deleteAllKeyframes()
+        abcXform.parm('fps').setExpression('$FPS')
+        abcXform.parm('objectPath').set(camXformPath)
+        cam = abcXform.createNode('cam', shapeName)
+        cam.setInput(0, abcXform.indirectInputs()[0])
+
+        focalPyExps = '__import__("_alembic_hom_extensions")\
+        .alembicGetCameraDict(hou.parent().parm("fileName").eval(),\
+         "%s", hou.frame()/hou.fps()).get("focal")' % camShapePath
+        aperturePyExps = '__import__("_alembic_hom_extensions")\
+        .alembicGetCameraDict(hou.parent().parm("fileName").eval(),\
+         "%s", hou.frame()/hou.fps()).get("aperture") \
+        *max(1, (hou.pwd().parm("resx").eval()/hou.pwd().parm("resy")\
+        .eval()*hou.pwd().parm("aspect").eval())*3./4) ' % camShapePath
+        cam.parm('focal').setExpression(
+            focalPyExps, language=hou.exprLanguage.Python)
+        cam.parm('aperture').setExpression(
+            aperturePyExps, language=hou.exprLanguage.Python)
+
+        if not importAsBakedCam:
+            return abcXform
+        else:
+            if scaleFactor != 1:
+                nullScale = abcXform.createInputNode(0, 'null')
+                nullScale.parm('scale').set(scaleFactor)
+            bakedCam = Connector.bakeAnim(
+                cam,
+                [hou.playbar.playbackRange()[0],
+                    hou.playbar.playbackRange()[1]], bakeParms=[
+                        'tx', 'ty', 'tz', 'rx', 'ry', 'rz', 'sx', 'sy', 'sz',
+                        'focal', 'aperture', 'resx', 'resy'], byChop=True)
+            if scaleFactor != 1:
+                nullScale.destroy()
+            if resolution != []:
+                for parmName in ['resx', 'resy']:
+                    bakedCam.parm(parmName).deleteAllKeyframes()
+                for parmName, res in zip(['resx', 'resy'], resolution):
+                    bakedCam.parm(parmName).set(res)
+                for parmName in ['resx', 'resy']:
+                    bakedCam.parm(parmName).lock(True)
+            abcXform.destroy()
+            bakedCam.setName(trsName, unique_name=True)
+            return {'status':True, 'message':'Cam imported ' + bakedCam.name(), 'output':bakedCam}
+
+    @staticmethod
+    def setTimeLine():
+        '''Set time line to FS , FE environment values'''
+        import ftrack
+        start_frame = float(os.getenv('FS'))
+        end_frame = float(os.getenv('FE'))
+        shot_id = os.getenv('FTRACK_SHOTID')
+        shot = ftrack.Shot(id=shot_id)
+        handles = float(shot.get('handles'))
+        fps = shot.get('fps')
+
+        print 'setting timeline to %s %s ' % (start_frame, end_frame)
+
+        # add handles to start and end frame
+        hsf = start_frame - handles
+        hef = end_frame + handles
+
+        hou.setFps(fps)
+        hou.setFrame(hsf)
+
+        try:
+            if start_frame != end_frame:
+                hou.hscript("tset {0} {1}".format(hsf / fps,
+                            hef / fps))
+                hou.playbar.setPlaybackRange(hsf, hef)
+        except IndexError:
+            pass
+
+    @staticmethod
     def getAssets():
+        '''Return the available assets in scene, return the *componentId(s)*'''
         componentIds = []
+
+        for n in hou.node('/').allSubChildren():
+            if n.parmTemplateGroup().findFolder('ftrack'):
+                valueftrackId = n.parm('componentId').eval()
+                if valueftrackId != '':
+                    if 'ftrack://' in valueftrackId:
+                        url = urlparse.urlparse(valueftrackId)
+                        valueftrackId = url.netloc
+                        componentIds.append((valueftrackId, n.name()))
+
+        # print componentIds
         return componentIds
 
     @staticmethod
     def getFileName():
-        return 'dummyTestScene'
+        '''Return the *current scene* name'''
+        return hou.hipFile.path()
 
     @staticmethod
     def getMainWindow():
@@ -35,10 +203,11 @@ class Connector(maincon.Connector):
 
     @staticmethod
     def importAsset(iAObj):
-        iAObj.assetName = iAObj.assetType.upper() + "_" + iAObj.assetName + "_AST"
+        iAObj.assetName = "%s_%s_AST" % (
+            iAObj.assetType.upper(),
+            iAObj.assetName)
         # Check if this AssetName already exists in scene
-        # iAObj.assetName = Connector.getUniqueSceneName(iAObj.assetName)
-
+        # Need to Done!
         assetHandler = FTAssetHandlerInstance.instance()
         importAsset = assetHandler.getAssetClass(iAObj.assetType)
         if importAsset:
@@ -49,47 +218,60 @@ class Connector(maincon.Connector):
 
     @staticmethod
     def selectObject(applicationObject=''):
-        print applicationObject
-        pass
+        for n in hou.node('/').allSubChildren():
+            if n.name() == applicationObject:
+                n.setSelected(1, True)
 
     @staticmethod
     def selectObjects(selection):
-        print selection
-        pass
+        '''Return the selected nodes'''
+        return hou.selectedNodes()
 
     @staticmethod
     def removeObject(applicationObject=''):
-        print applicationObject
-        pass
+        for n in hou.node('/').allSubChildren():
+            if n.name() == applicationObject:
+                n.destroy()
 
     @staticmethod
     def changeVersion(applicationObject=None, iAObj=None):
-        if iAObj.assetType in ['rig', 'light']:
-            changeAsset = houdiniassets.GenericAsset()
-        elif iAObj.assetType in ['geo']:
-            changeAsset = houdiniassets.GeometryAsset()
+        assetHandler = FTAssetHandlerInstance.instance()
+        changeAsset = assetHandler.getAssetClass(iAObj.assetType)
+        if changeAsset:
+            result = changeAsset.changeVersion(
+                iAObj, applicationObject
+            )
+            return result
         else:
             print 'assetType not supported'
             return False
 
-        result = changeAsset.changeVersion(iAObj, applicationObject)
-        return result
-
     @staticmethod
     def getSelectedObjects():
-        selectedObjects = ['horse', 'pig', 'donkey', 'cat', 'dog']
-        print selectedObjects
-        return selectedObjects
+        return hou.selectedNodes()
 
     @staticmethod
     def getSelectedAssets():
-        selectedObjects = ['horse', 'pig', 'donkey']
-        print selectedObjects
+        selection = hou.selectedNodes()
+        selectedObjects = []
+        for node in selection:
+            try:
+                node.parm('componentId').eval()
+                selectedObjects.append(node.name())
+            except:
+                pass
         return selectedObjects
 
     @staticmethod
     def setNodeColor(applicationObject='', latest=True):
-        pass
+        latestColor = hou.Color(hou.Vector3(0.07, 0.63, 0.29))
+        oldColor = hou.Color(hou.Vector3(0.89, 0.39, 0.08))
+        for n in hou.node('/').allSubChildren():
+            if n.name() == applicationObject:
+                if latest:
+                    n.setColor(latestColor)
+                else:
+                    n.setColor(oldColor)
 
     @staticmethod
     def publishAsset(iAObj=None):
@@ -116,8 +298,20 @@ class Connector(maincon.Connector):
 
     @staticmethod
     def getUniqueSceneName(assetName):
-        print assetName
-        return assetName
+        assetName = assetName
+        for n in hou.node('/').allSubChildren():
+            if n.name().startswith(
+                    HelpFunctions.safeString(assetName)):
+                if n:
+                    i = 0
+                    while n:
+                        uniqueAssetName = assetName + str(i)
+                        if n.name().startswith(
+                                HelpFunctions.safeString(uniqueAssetName)):
+                            i = i + 1
+                    return uniqueAssetName
+                else:
+                    return assetName
 
     @staticmethod
     def getReferenceNode(assetLink):
@@ -133,11 +327,7 @@ class Connector(maincon.Connector):
 
     @staticmethod
     def executeInThread(function, arg):
-        '''Execute the given *function* with provided *args* in a separate thread
-        '''
-        # import houdini.utils
-        # houdini.utils.executeInMainThreadWithResult(function, arg)
-        pass
+        function(arg)
 
     # Make certain scene validations before actualy publishing
     @classmethod
@@ -175,7 +365,8 @@ class Connector(maincon.Connector):
         desktop = hou.ui.curDesktop()
         scene_view = toolutils.sceneViewer()
 
-        if scene_view is None or scene_view.type() != hou.paneTabType.SceneViewer:
+        if scene_view is None or (
+                scene_view.type() != hou.paneTabType.SceneViewer):
             raise hou.Error("No scene view available to flipbook")
         viewport = scene_view.curViewport()
 
@@ -188,8 +379,6 @@ class Connector(maincon.Connector):
 
         executeCommand = "viewwrite -c -f 0 1 -r %s %s %s %s" % (
             res[0], res[1], view, filename)
-
-        print executeCommand
 
         hou.hscript(executeCommand)
         return filename
